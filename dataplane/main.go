f31da0ff2dd5e755071a5b25c26f1eec1d22826a
Shane Utt <shaneutt@linux.com>
2022-11-01 16:02:21 -0400

    chore: load dummy xdp prog on dest interface

--- dataplane/main.go
+++ dataplane/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"strings"
 
+	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf xdp.c -- -I../headers
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalf("Please specify a network interface")
+	if len(os.Args) < 3 {
+		log.Fatalf("Please specify a main and destination network interface")
 	}
 
 	ifaceName := os.Args[1]
 	iface, err := net.InterfaceByName(ifaceName)
 	if err != nil {
 		log.Fatalf("lookup network iface %q: %s", ifaceName, err)
+	}
+	ifaceName2 := os.Args[2]
+	iface2, err := net.InterfaceByName(ifaceName2)
+	if err != nil {
+		log.Fatalf("lookup network iface %s: %s", ifaceName, err)
 	}
 
 	objs := bpfObjects{}
@@ -35,6 +45,15 @@ func main() {
 		log.Fatalf("could not attach XDP program: %s", err)
 	}
 	defer l.Close()
+
+	l2, err := link.AttachXDP(link.XDPOptions{
+		Program:   objs.BpfRedirectPlaceholder,
+		Interface: iface2.Index,
+	})
+	if err != nil {
+		log.Fatalf("could not attach XDP program: %s", err)
+	}
+	defer l2.Close()
 
 	log.Printf("Attached XDP program to iface %q (index %d)", iface.Name, iface.Index)
 	log.Printf("Press Ctrl-C to exit and remove the program")
