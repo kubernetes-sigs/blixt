263eaf519a750e336c1a2deed2831cdb8600842d
Andrew Stoycos <astoycos@redhat.com>
2022-11-14 09:49:50 -0500

    Add bpf_map struct definitions
    Add types Backend and BackendKey
    which are used in the BackendsMap
    which is ultimately used by our
    TC ingress program.
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- dataplane/common/src/lib.rs
+++ dataplane/common/src/lib.rs
@@ -1,1 +1,22 @@
 #![no_std]
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct Backend {
+    pub daddr: u32,
+    pub dport: u32,
+    pub ifindex: u16,
+}
+
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for Backend {}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct BackendKey {
+    pub ip: u32,
+    pub port: u32,
+}
+
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for BackendKey {}
