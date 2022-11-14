8a35c4455c61982f859bbd557c5213402df6719d
Andrew Stoycos <astoycos@redhat.com>
2022-11-14 12:25:47 -0500

    Add TC_INGRESS UDP redirect program (#23)
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- dataplane/xtask/src/main.rs
+++ dataplane/xtask/src/main.rs
@@ -1,5 +1,7 @@
 mod build_ebpf;
 mod run;
+// Remember to run `cargo install bindgen-cli`
+mod codegen;
 
 use std::process::exit;
 
@@ -15,6 +17,7 @@ pub struct Options {
 enum Command {
     BuildEbpf(build_ebpf::Options),
     Run(run::Options),
+    Codegen,
 }
 
 fn main() {
@@ -24,6 +27,7 @@ fn main() {
     let ret = match opts.command {
         BuildEbpf(opts) => build_ebpf::build_ebpf(opts),
         Run(opts) => run::run(opts),
+        Codegen => codegen::generate(),
     };
 
     if let Err(e) = ret {
