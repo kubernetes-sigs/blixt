c537b7de45fe2430efeb7b66864ea1ca7afaadeb
Mattia Lavacca <lavacca.mattia@gmail.com>
2022-12-07 17:09:11 +0100

    listener status update (#19)
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>
    Co-authored-by: Shane Utt <shane@shaneutt.com>

---
+++ controllers/gateway_controller_status.go
@@ -0,0 +1,200 @@
+package controllers
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+)
+
+// updateGatewayStatus computes the new Gateway status, setting its ready condition and all the
+// ready listeners's ready conditions to true, unless a resolvedRefs error is discovered. In
+// that case, the proper listener ready condition and the gateway one are set to false.
+// The addresses are updated as well.
+func updateGatewayStatus(ctx context.Context, gateway *gatewayv1beta1.Gateway, svc *corev1.Service) {
+	// gateway addresses
+	gwaddrs := make([]gatewayv1beta1.GatewayAddress, 0, len(svc.Status.LoadBalancer.Ingress))
+	for _, addr := range svc.Status.LoadBalancer.Ingress {
+		if addr.IP != "" {
+			gwaddrs = append(gwaddrs, gatewayv1beta1.GatewayAddress{
+				Type:  &ipAddrType,
+				Value: addr.IP,
+			})
+		}
+		if addr.Hostname != "" {
+			gwaddrs = append(gwaddrs, gatewayv1beta1.GatewayAddress{
+				Type:  &hostAddrType,
+				Value: addr.Hostname,
+			})
+		}
+	}
+	gateway.Status.Addresses = gwaddrs
+
+	// gateway conditions
+	newGatewayCondition := metav1.Condition{
+		Type:               string(gatewayv1beta1.GatewayConditionReady),
+		Status:             metav1.ConditionTrue,
+		Reason:             string(gatewayv1beta1.GatewayReasonReady),
+		ObservedGeneration: gateway.Generation,
+		LastTransitionTime: metav1.Now(),
+		Message:            "the gateway is ready to route traffic",
+	}
+
+	// gateway listeners conditions
+	listenersStatus := make([]gatewayv1beta1.ListenerStatus, 0, len(gateway.Spec.Listeners))
+	for _, l := range gateway.Spec.Listeners {
+		supportedKinds, resolvedRefsCondition := getSupportedKinds(gateway.Generation, l)
+		listenerReadyStatus := corev1.ConditionTrue
+		listenerReadyReason := gatewayv1beta1.ListenerReasonReady
+		if resolvedRefsCondition.Status == metav1.ConditionFalse {
+			listenerReadyStatus = corev1.ConditionStatus(metav1.ConditionFalse)
+			listenerReadyReason = gatewayv1beta1.ListenerReasonResolvedRefs
+		}
+		listenersStatus = append(listenersStatus, gatewayv1beta1.ListenerStatus{
+			Name:           l.Name,
+			SupportedKinds: supportedKinds,
+			Conditions: []metav1.Condition{
+				{
+					Type:               string(gatewayv1beta1.ListenerConditionReady),
+					Status:             metav1.ConditionStatus(listenerReadyStatus),
+					Reason:             string(listenerReadyReason),
+					ObservedGeneration: gateway.Generation,
+					LastTransitionTime: metav1.Now(),
+				},
+				resolvedRefsCondition,
+			},
+		})
+		if resolvedRefsCondition.Status == metav1.ConditionFalse {
+			newGatewayCondition.Status = metav1.ConditionFalse
+			newGatewayCondition.Reason = string(gatewayv1beta1.GatewayReasonListenersNotReady)
+			newGatewayCondition.Message = "the gateway is not ready to route traffic"
+		}
+	}
+
+	gateway.Status.Conditions = []metav1.Condition{
+		newGatewayCondition,
+	}
+	gateway.Status.Listeners = listenersStatus
+}
+
+// initGatewayStatus initializes the GatewayStatus, setting the ready condition to
+// not ready and all the listeners ready status to not ready as well.
+func initGatewayStatus(gateway *gatewayv1beta1.Gateway) {
+	gateway.Status = gatewayv1beta1.GatewayStatus{
+		Conditions: []metav1.Condition{
+			{
+				Type:               string(gatewayv1beta1.GatewayConditionReady),
+				Status:             metav1.ConditionFalse,
+				Reason:             string(gatewayv1beta1.GatewayReasonListenersNotReady),
+				ObservedGeneration: gateway.Generation,
+				LastTransitionTime: metav1.Now(),
+				Message:            "the gateway is not ready to route traffic",
+			},
+		},
+	}
+	gateway.Status.Listeners = make([]gatewayv1beta1.ListenerStatus, 0, len(gateway.Spec.Listeners))
+	for _, l := range gateway.Spec.Listeners {
+		supportedKinds, resolvedRefsCondition := getSupportedKinds(gateway.Generation, l)
+		gateway.Status.Listeners = append(gateway.Status.Listeners, gatewayv1beta1.ListenerStatus{
+			Name:           l.Name,
+			SupportedKinds: supportedKinds,
+			Conditions: []metav1.Condition{
+				{
+					Type:               string(gatewayv1beta1.ListenerConditionReady),
+					Status:             metav1.ConditionFalse,
+					Reason:             string(gatewayv1beta1.ListenerReasonPending),
+					ObservedGeneration: gateway.Generation,
+					LastTransitionTime: metav1.Now(),
+				},
+				resolvedRefsCondition,
+			},
+		})
+	}
+}
+
+func getSupportedKinds(generation int64, listener gatewayv1beta1.Listener) (supportedKinds []gatewayv1beta1.RouteGroupKind, resolvedRefsCondition metav1.Condition) {
+	supportedKinds = make([]gatewayv1beta1.RouteGroupKind, 0)
+	resolvedRefsCondition = metav1.Condition{
+		Type:               string(gatewayv1beta1.ListenerConditionResolvedRefs),
+		Status:             metav1.ConditionTrue,
+		Reason:             string(gatewayv1beta1.ListenerReasonResolvedRefs),
+		ObservedGeneration: generation,
+		LastTransitionTime: metav1.Now(),
+	}
+	if len(listener.AllowedRoutes.Kinds) == 0 {
+		// When unspecified or empty, the kinds of Routes selected are determined using the Listener protocol.
+		switch listener.Protocol {
+		case gatewayv1beta1.TCPProtocolType:
+			supportedKinds = append(supportedKinds, gatewayv1beta1.RouteGroupKind{
+				Group: (*gatewayv1beta1.Group)(&gatewayv1beta1.GroupVersion.Group),
+				Kind:  "TCPRoute",
+			})
+		case gatewayv1beta1.UDPProtocolType:
+			supportedKinds = append(supportedKinds, gatewayv1beta1.RouteGroupKind{
+				Group: (*gatewayv1beta1.Group)(&gatewayv1beta1.GroupVersion.Group),
+				Kind:  "UDPRoute",
+			})
+		default:
+			resolvedRefsCondition.Status = metav1.ConditionFalse
+			resolvedRefsCondition.Reason = string(gatewayv1beta1.ListenerReasonInvalidRouteKinds)
+		}
+	}
+
+	for _, k := range listener.AllowedRoutes.Kinds {
+		if (k.Group != nil && *k.Group != "" && *k.Group != gatewayv1beta1.Group(gatewayv1beta1.GroupVersion.Group)) ||
+			(k.Kind != "UDPRoute" && k.Kind != "TCPRoute") {
+			resolvedRefsCondition.Status = metav1.ConditionFalse
+			resolvedRefsCondition.Reason = string(gatewayv1beta1.ListenerReasonInvalidRouteKinds)
+			continue
+		}
+		supportedKinds = append(supportedKinds, gatewayv1beta1.RouteGroupKind{
+			Group: k.Group,
+			Kind:  k.Kind,
+		})
+	}
+	return supportedKinds, resolvedRefsCondition
+}
+
+// factorizeStatus takes the old gateway conditions not transitioned and copies them
+// into the new gateway status, so that only the transitioning conditions gets actually patched.
+func factorizeStatus(gateway, oldGateway *gatewayv1beta1.Gateway) {
+	for i, c := range gateway.Status.Conditions {
+		for _, oldC := range oldGateway.Status.Conditions {
+			if c.Type == oldC.Type {
+				if c.Status == oldC.Status && c.Reason == oldC.Reason {
+					gateway.Status.Conditions[i] = oldC
+				}
+			}
+		}
+	}
+
+	for i, l := range gateway.Status.Listeners {
+		for j, lc := range l.Conditions {
+			for _, ol := range oldGateway.Status.Listeners {
+				if ol.Name != l.Name {
+					continue
+				}
+				for _, olc := range ol.Conditions {
+					if lc.Type == olc.Type {
+						if lc.Status == olc.Status && lc.Reason == olc.Reason {
+							gateway.Status.Listeners[i].Conditions[j] = olc
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+// isGatewayReady returns two boolean values:
+// - the status of the ready condition
+// - a boolean flag to check if the condition exists
+func isGatewayReady(gateway *gatewayv1beta1.Gateway) (status bool, isSet bool) {
+	for _, c := range gateway.Status.Conditions {
+		if c.Type == string(gatewayv1beta1.GatewayConditionReady) {
+			return c.Status == metav1.ConditionTrue, true
+		}
+	}
+	return false, false
+}
--- controllers/gateway_controller_test.go
+++ controllers/gateway_controller_test.go
@@ -1,16 +1,27 @@
 package controllers
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	controllerruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	fakectrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 
 	"github.com/kong/blixt/internal/test/utils"
+	"github.com/kong/blixt/pkg/vars"
 )
+
+func init() {
+	_ = gatewayv1beta1.AddToScheme(scheme.Scheme)
+}
 
 func TestGatewayReconciler_gatewayHasMatchingGatewayClass(t *testing.T) {
 	logger, output := utils.NewBytesBufferLogger()
@@ -23,7 +34,7 @@ func TestGatewayReconciler_gatewayHasMatchingGatewayClass(t *testing.T) {
 
 	for _, tt := range []struct {
 		name             string
-		obj              client.Object
+		obj              controllerruntimeclient.Object
 		expected         bool
 		logEntryExpected string
 	}{
@@ -88,5 +99,358 @@ func TestGatewayReconciler_gatewayHasMatchingGatewayClass(t *testing.T) {
 		})
 
 		output.Reset()
+	}
+}
+
+func TestGatewayReconciler_reconcile(t *testing.T) {
+	testCases := []struct {
+		name         string
+		gatewayReq   reconcile.Request
+		gatewayClass *gatewayv1beta1.GatewayClass
+		gateway      *gatewayv1beta1.Gateway
+		objectsToAdd []controllerruntimeclient.Object
+
+		run func(t *testing.T, reconciler GatewayReconciler, gatewayReq reconcile.Request, gatewayClass *gatewayv1beta1.Gateway)
+	}{
+		{
+			name: "gatewayclass not accepted",
+			gatewayReq: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+			},
+			gatewayClass: &gatewayv1beta1.GatewayClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-gatewayclass",
+				},
+				Spec: gatewayv1beta1.GatewayClassSpec{
+					ControllerName: gatewayv1beta1.GatewayController("mismatch-controller-name"),
+				},
+			},
+			gateway: &gatewayv1beta1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+				Spec: gatewayv1beta1.GatewaySpec{
+					GatewayClassName: "test-gatewayclass",
+					Listeners: []gatewayv1beta1.Listener{
+						{
+							Name:     "udp",
+							Protocol: gatewayv1beta1.UDPProtocolType,
+							Port:     9875,
+						},
+					},
+				},
+			},
+			run: func(t *testing.T, reconciler GatewayReconciler, gatewayReq reconcile.Request, gateway *gatewayv1beta1.Gateway) {
+				ctx := context.Background()
+				_, err := reconciler.Reconcile(ctx, gatewayReq)
+				require.NoError(t, err)
+				newGateway := &gatewayv1beta1.Gateway{}
+				err = reconciler.Client.Get(ctx, gatewayReq.NamespacedName, newGateway)
+				require.NoError(t, err)
+				require.Len(t, newGateway.Status.Conditions, 0)
+				require.Len(t, newGateway.Status.Listeners, 0)
+				require.Len(t, newGateway.Status.Addresses, 0)
+			},
+		},
+		{
+			name: "gatewayclass accepted, gateway ready",
+			gatewayReq: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+			},
+			gatewayClass: &gatewayv1beta1.GatewayClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-gatewayclass",
+				},
+				Spec: gatewayv1beta1.GatewayClassSpec{
+					ControllerName: vars.GatewayClassControllerName,
+				},
+			},
+			gateway: &gatewayv1beta1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+				Spec: gatewayv1beta1.GatewaySpec{
+					GatewayClassName: "test-gatewayclass",
+					Listeners: []gatewayv1beta1.Listener{
+						{
+							Name:          "udp",
+							Protocol:      gatewayv1beta1.UDPProtocolType,
+							Port:          9875,
+							AllowedRoutes: &gatewayv1beta1.AllowedRoutes{},
+						},
+					},
+				},
+			},
+			objectsToAdd: []controllerruntimeclient.Object{
+				&corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "test-namespace",
+						Name:      "service-for-gateway-test-gateway",
+						Labels: map[string]string{
+							gatewayServiceLabel: "test-gateway",
+						},
+					},
+					Spec: corev1.ServiceSpec{
+						Type:      corev1.ServiceTypeLoadBalancer,
+						ClusterIP: "1.1.1.1",
+						Ports: []corev1.ServicePort{
+							{
+								Name:     "udp",
+								Protocol: corev1.ProtocolUDP,
+								Port:     9875,
+							},
+						},
+					},
+					Status: corev1.ServiceStatus{
+						LoadBalancer: corev1.LoadBalancerStatus{
+							Ingress: []corev1.LoadBalancerIngress{
+								{
+									IP: "1.2.3.4",
+								},
+							},
+						},
+					},
+				},
+				&corev1.Endpoints{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "service-for-gateway-test-gateway",
+						Namespace: "test-namespace",
+					},
+				},
+			},
+			run: func(t *testing.T, reconciler GatewayReconciler, gatewayReq reconcile.Request, gateway *gatewayv1beta1.Gateway) {
+				ctx := context.Background()
+				// first reconcile to initialize the Gateway status
+				_, err := reconciler.Reconcile(ctx, gatewayReq)
+				require.NoError(t, err)
+				// second reconcile to have a complete status
+				_, err = reconciler.Reconcile(ctx, gatewayReq)
+				require.NoError(t, err)
+				newGateway := &gatewayv1beta1.Gateway{}
+				err = reconciler.Client.Get(ctx, gatewayReq.NamespacedName, newGateway)
+				require.NoError(t, err)
+				require.Len(t, newGateway.Status.Addresses, 1)
+				require.Len(t, newGateway.Status.Conditions, 1)
+				require.Equal(t, newGateway.Status.Conditions[0].Status, metav1.ConditionTrue)
+				require.Len(t, newGateway.Status.Listeners, 1)
+				require.Equal(t, newGateway.Status.Listeners[0].SupportedKinds, []gatewayv1beta1.RouteGroupKind{
+					{
+						Group: (*gatewayv1beta1.Group)(&gatewayv1beta1.GroupVersion.Group),
+						Kind:  "UDPRoute",
+					},
+				})
+				for _, c := range newGateway.Status.Listeners[0].Conditions {
+					require.Equal(t, c.Status, metav1.ConditionTrue)
+				}
+
+			},
+		},
+		{
+			name: "gatewayclass accepted, gateway not ready because resources are missing",
+			gatewayReq: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+			},
+			gatewayClass: &gatewayv1beta1.GatewayClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-gatewayclass",
+				},
+				Spec: gatewayv1beta1.GatewayClassSpec{
+					ControllerName: vars.GatewayClassControllerName,
+				},
+			},
+			gateway: &gatewayv1beta1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+				Spec: gatewayv1beta1.GatewaySpec{
+					GatewayClassName: "test-gatewayclass",
+					Listeners: []gatewayv1beta1.Listener{
+						{
+							Name:          "udp",
+							Protocol:      gatewayv1beta1.UDPProtocolType,
+							Port:          9875,
+							AllowedRoutes: &gatewayv1beta1.AllowedRoutes{},
+						},
+					},
+				},
+			},
+			run: func(t *testing.T, reconciler GatewayReconciler, gatewayReq reconcile.Request, gateway *gatewayv1beta1.Gateway) {
+				ctx := context.Background()
+				_, err := reconciler.Reconcile(ctx, gatewayReq)
+				require.NoError(t, err)
+				newGateway := &gatewayv1beta1.Gateway{}
+				err = reconciler.Client.Get(ctx, gatewayReq.NamespacedName, newGateway)
+				require.NoError(t, err)
+				require.Len(t, newGateway.Status.Addresses, 0)
+				require.Len(t, newGateway.Status.Conditions, 1)
+				require.Equal(t, newGateway.Status.Conditions[0].Status, metav1.ConditionFalse)
+				require.Len(t, newGateway.Status.Listeners, 1)
+				require.Equal(t, newGateway.Status.Listeners[0].SupportedKinds, []gatewayv1beta1.RouteGroupKind{
+					{
+						Group: (*gatewayv1beta1.Group)(&gatewayv1beta1.GroupVersion.Group),
+						Kind:  "UDPRoute",
+					},
+				})
+				for _, c := range newGateway.Status.Listeners[0].Conditions {
+					if c.Type == string(gatewayv1beta1.ListenerConditionResolvedRefs) {
+						require.Equal(t, c.Status, metav1.ConditionTrue)
+					} else {
+						require.Equal(t, c.Status, metav1.ConditionFalse)
+					}
+				}
+			},
+		},
+		{
+			name: "gatewayclass accepted, gateway not ready because resolvedrefs is false",
+			gatewayReq: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+			},
+			gatewayClass: &gatewayv1beta1.GatewayClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-gatewayclass",
+				},
+				Spec: gatewayv1beta1.GatewayClassSpec{
+					ControllerName: vars.GatewayClassControllerName,
+				},
+			},
+			gateway: &gatewayv1beta1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-gateway",
+					Namespace: "test-namespace",
+				},
+				Spec: gatewayv1beta1.GatewaySpec{
+					GatewayClassName: "test-gatewayclass",
+					Listeners: []gatewayv1beta1.Listener{
+						{
+							Name:          "http",
+							Protocol:      gatewayv1beta1.HTTPProtocolType,
+							Port:          9875,
+							AllowedRoutes: &gatewayv1beta1.AllowedRoutes{},
+						},
+						{
+							Name:          "udp",
+							Protocol:      gatewayv1beta1.UDPProtocolType,
+							Port:          9875,
+							AllowedRoutes: &gatewayv1beta1.AllowedRoutes{},
+						},
+					},
+				},
+			},
+			objectsToAdd: []controllerruntimeclient.Object{
+				&corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "test-namespace",
+						Name:      "service-for-gateway-test-gateway",
+						Labels: map[string]string{
+							gatewayServiceLabel: "test-gateway",
+						},
+					},
+					Spec: corev1.ServiceSpec{
+						Type:      corev1.ServiceTypeLoadBalancer,
+						ClusterIP: "1.1.1.1",
+						Ports: []corev1.ServicePort{
+							{
+								Name:     "udp",
+								Protocol: corev1.ProtocolUDP,
+								Port:     9875,
+							},
+						},
+					},
+					Status: corev1.ServiceStatus{
+						LoadBalancer: corev1.LoadBalancerStatus{
+							Ingress: []corev1.LoadBalancerIngress{
+								{
+									IP: "1.2.3.4",
+								},
+							},
+						},
+					},
+				},
+				&corev1.Endpoints{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "service-for-gateway-test-gateway",
+						Namespace: "test-namespace",
+					},
+				},
+			},
+			run: func(t *testing.T, reconciler GatewayReconciler, gatewayReq reconcile.Request, gateway *gatewayv1beta1.Gateway) {
+				ctx := context.Background()
+				// first reconcile to initialize the Gateway status
+				_, err := reconciler.Reconcile(ctx, gatewayReq)
+				require.NoError(t, err)
+				// second reconcile to have a complete status
+				_, err = reconciler.Reconcile(ctx, gatewayReq)
+				require.NoError(t, err)
+				newGateway := &gatewayv1beta1.Gateway{}
+				err = reconciler.Client.Get(ctx, gatewayReq.NamespacedName, newGateway)
+				require.NoError(t, err)
+				require.Len(t, newGateway.Status.Addresses, 1)
+				require.Len(t, newGateway.Status.Conditions, 1)
+				require.Equal(t, newGateway.Status.Conditions[0].Status, metav1.ConditionFalse)
+				require.Len(t, newGateway.Status.Listeners, 2)
+				for _, l := range newGateway.Status.Listeners {
+					if l.Name == "http" {
+						require.Len(t, l.SupportedKinds, 0)
+						for _, c := range l.Conditions {
+							if c.Type == string(gatewayv1beta1.ListenerConditionResolvedRefs) {
+								require.Equal(t, c.Status, metav1.ConditionFalse)
+							} else {
+								require.Equal(t, c.Status, metav1.ConditionFalse)
+							}
+						}
+					}
+					if l.Name == "udp" {
+						require.Equal(t, l.SupportedKinds, []gatewayv1beta1.RouteGroupKind{
+							{
+								Group: (*gatewayv1beta1.Group)(&gatewayv1beta1.GroupVersion.Group),
+								Kind:  "UDPRoute",
+							},
+						})
+						for _, c := range l.Conditions {
+							require.Equal(t, c.Status, metav1.ConditionTrue)
+						}
+					}
+				}
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			objectsToAdd := []controllerruntimeclient.Object{
+				tc.gatewayClass,
+				tc.gateway,
+			}
+			objectsToAdd = append(objectsToAdd, tc.objectsToAdd...)
+
+			fakeClient := fakectrlruntimeclient.
+				NewClientBuilder().
+				WithScheme(scheme.Scheme).
+				WithObjects(objectsToAdd...).
+				Build()
+
+			reconciler := GatewayReconciler{
+				Client: fakeClient,
+			}
+
+			tc.run(t, reconciler, tc.gatewayReq, tc.gateway)
+		})
 	}
 }
