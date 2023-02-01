cdbcb24c0b16022d709ec7d10a82297e79a1c6bb
Andrew Stoycos <astoycos@redhat.com>
2023-02-01 17:13:02 -0500

    Add UDP roundtrip test
    Add a new test to ensure icmp destination unreachable
    packets are directed back to an external client from
    the destination pod.
    
    Update our cleanup mechanism to be able to have different
    cleanups for each test individually.
    
    Ensure we build the udp-test-server image in CI so local
    changes can be picked up there.
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- test/integration/udproute_test.go
+++ test/integration/udproute_test.go
@@ -11,6 +11,7 @@ import (
 	"net"
 	"testing"
 	"time"
+	"os"
 
 	"github.com/google/uuid"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
@@ -21,17 +22,90 @@ import (
 )
 
 const (
-	udprouteSampleKustomize = "../../config/samples/udproute"
-	udprouteSampleName      = "blixt-udproute-sample"
+	udprouteSampleKustomize        = "../../config/samples/udproute"
+	udproutenoreachSampleKustomize = "../../config/samples/udproute-noreach"
+	udprouteSampleName             = "blixt-udproute-sample"
 )
 
 func TestUDPRouteBasics(t *testing.T) {
+	udpRouteBasicsCleanupKey := "udproutebasics"
+	defer runCleanup(udpRouteBasicsCleanupKey)
+
 	t.Log("deploying config/samples/udproute kustomize")
 	require.NoError(t, clusters.KustomizeDeployForCluster(ctx, env.Cluster(), udprouteSampleKustomize))
-	addCleanup(func(ctx context.Context) error {
+	addCleanup(udpRouteBasicsCleanupKey, func(ctx context.Context) error {
 		cleanupLog("cleaning up config/samples/udproute kustomize")
 		return clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), udprouteSampleKustomize)
 	})
+
+	t.Log("waiting for Gateway to have an address")
+	var gw *gatewayv1beta1.Gateway
+	require.Eventually(t, func() bool {
+		var err error
+		gw, err = gwclient.GatewayV1beta1().Gateways(corev1.NamespaceDefault).Get(ctx, udprouteSampleName, metav1.GetOptions{})
+		require.NoError(t, err)
+		return len(gw.Status.Addresses) > 0
+	}, time.Minute, time.Second)
+	require.NotNil(t, gw.Status.Addresses[0].Type)
+	require.Equal(t, gatewayv1beta1.IPAddressType, *gw.Status.Addresses[0].Type)
+	gwaddr := fmt.Sprintf("%s:9875", gw.Status.Addresses[0].Value)
+
+	t.Log("waiting for udp server to be available")
+	require.Eventually(t, func() bool {
+		server, err := env.Cluster().Client().AppsV1().Deployments(corev1.NamespaceDefault).Get(ctx, udprouteSampleName, metav1.GetOptions{})
+		require.NoError(t, err)
+		return server.Status.AvailableReplicas > 0
+	}, time.Minute, time.Second)
+	t.Logf("sending a datagram to the UDP server at %s", gwaddr)
+	message := uuid.NewString()
+	conn, err := net.Dial("udp", gwaddr)
+	require.NoError(t, err)
+	defer conn.Close()
+	bytesWritten, err := conn.Write([]byte(message))
+	require.NoError(t, err)
+	require.Equal(t, len(message), bytesWritten)
+
+	// ensure server sent nothing back
+	t.Logf("ensuring UDP server sent nothing back")
+	err = conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+	require.NoError(t, err)
+	_, err = conn.Read(make([]byte, 2048))
+	require.ErrorIs(t, err, os.ErrDeadlineExceeded)
+
+	t.Logf("%d bytes written to the UDP server, verifying receipt", bytesWritten)
+	pods, err := env.Cluster().Client().CoreV1().Pods(corev1.NamespaceDefault).List(ctx, metav1.ListOptions{LabelSelector: fmt.Sprintf("app=%s", udprouteSampleName)})
+	require.NoError(t, err)
+	require.Len(t, pods.Items, 1)
+	udpServerPod := pods.Items[0]
+	req := env.Cluster().Client().CoreV1().Pods(corev1.NamespaceDefault).GetLogs(udpServerPod.Name, &corev1.PodLogOptions{})
+	logs, err := req.Stream(ctx)
+	require.NoError(t, err)
+	defer logs.Close()
+	output := new(bytes.Buffer)
+	_, err = io.Copy(output, logs)
+	require.NoError(t, err)
+	require.Contains(t, output.String(), message)
+}
+
+func TestUDPRouteNoReach(t *testing.T) {
+	udpRouteNoReachCleanupKey := "udproutenoreach"
+	defer runCleanup(udpRouteNoReachCleanupKey)
+
+	t.Log("deploying config/samples/udproute-noreach kustomize")
+	require.NoError(t, clusters.KustomizeDeployForCluster(ctx, env.Cluster(), udproutenoreachSampleKustomize))
+	addCleanup(udpRouteNoReachCleanupKey, func(ctx context.Context) error {
+		cleanupLog("cleaning up config/samples/udproute-noreach kustomize")
+		return clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), udproutenoreachSampleKustomize)
+	})
+
+	// TODO (astoycos) this currently won't work but it will with updated control-plane logic
+	// add it back so we don't have to maintain a whole new kustomize config for a one line change.
+	// // Update Server to ensure it's in dry run mode then wait for it to be ready
+	// server, err := env.Cluster().Client().AppsV1().Deployments(corev1.NamespaceDefault).Get(ctx, udprouteSampleName, metav1.GetOptions{})
+	// require.NoError(t, err)
+	// server.Spec.Template.Spec.Containers[0].Command = []string{"./udp-test-server", "--dry-run"}
+	// _, err = env.Cluster().Client().AppsV1().Deployments(corev1.NamespaceDefault).Update(ctx, server, metav1.UpdateOptions{})
+	// require.NoError(t, err)
 
 	t.Log("waiting for Gateway to have an address")
 	var gw *gatewayv1beta1.Gateway
@@ -61,17 +135,10 @@ func TestUDPRouteBasics(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, len(message), bytesWritten)
 
-	t.Logf("%d bytes written to the UDP server, verifying receipt", bytesWritten)
-	pods, err := env.Cluster().Client().CoreV1().Pods(corev1.NamespaceDefault).List(ctx, metav1.ListOptions{LabelSelector: fmt.Sprintf("app=%s", udprouteSampleName)})
+	// ensure server sent back icmp host unreachable
+	t.Logf("ensuring UDP server sent back icmp host unreachable")
+	err = conn.SetReadDeadline(time.Now().Add(10 * time.Second))
 	require.NoError(t, err)
-	require.Len(t, pods.Items, 1)
-	udpServerPod := pods.Items[0]
-	req := env.Cluster().Client().CoreV1().Pods(corev1.NamespaceDefault).GetLogs(udpServerPod.Name, &corev1.PodLogOptions{})
-	logs, err := req.Stream(ctx)
-	require.NoError(t, err)
-	defer logs.Close()
-	output := new(bytes.Buffer)
-	_, err = io.Copy(output, logs)
-	require.NoError(t, err)
-	require.Contains(t, output.String(), message)
+	_, err = conn.Read(make([]byte, 2048))
+	require.ErrorContains(t, err, "read: connection refused")
 }
