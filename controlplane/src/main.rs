681c153155c5dc4247d9abcde852aa6153cb25ce
Harald Gutmann <harald@gutmann.one>
2025-07-22 12:11:48 +0200

    fix(config): default deployment and pod startup issues
    add controlplane health check
    expose dataplane service port
    remove missing patch file
    
    Signed-off-by: Harald Gutmann <harald@gutmann.one>

--- controlplane/src/main.rs
+++ controlplane/src/main.rs
@@ -13,11 +13,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 use controlplane::*;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 
 use kube::Client;
+use tokio::task::JoinHandle;
 use tokio::try_join;
+use tonic::transport::Server;
 use tracing::*;
 
 #[tokio::main]
@@ -46,8 +48,35 @@ pub async fn run() {
     if let Err(error) = try_join!(
         gateway_controller(ctx.clone()),
         gatewayclass_controller(ctx),
+        setup_health_checks(IpAddr::from(Ipv4Addr::new(0, 0, 0, 0)), 8080)
     ) {
         error!("failed to start controllers: {error:?}");
         std::process::exit(1);
     }
+}
+
+// TODO: integrate with DataplaneClientManager connection status
+// only get healthy once the dataplane pod connections are established
+async fn setup_health_checks(addr: IpAddr, port: u16) -> Result<JoinHandle<()>> {
+    let healthchecks = tokio::spawn(async move {
+        let (_, health_service) = tonic_health::server::health_reporter();
+        let server_builder = Server::builder();
+
+        // by convention we add 1 to the API listen port and use that
+        // for the health check port.
+        let port = port + 1;
+
+        let addr = match addr {
+            IpAddr::V4(v4) => SocketAddr::V4(SocketAddrV4::new(v4, port)),
+            IpAddr::V6(v6) => SocketAddr::V6(SocketAddrV6::new(v6, port, 0, 0)),
+        };
+
+        let server = server_builder.serve(addr, health_service);
+
+        debug!("gRPC Health Checking service listens on {addr}");
+        server
+            .await
+            .expect("Failed to serve gRPC Health Checking service");
+    });
+    Ok(healthchecks)
 }
